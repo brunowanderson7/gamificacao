d603dd653af7b5bde83fde45aceffbdb3439da2e
brunowanderson7 <brunowanderson8@gmail.com>
2023-08-20 23:27:05 -0300

    premios names ok

---
+++ src/components/DataRoulette.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { api } from "@/lib/api";
+import { Roulette } from "./Roulette";
+import { useEffect, useState } from "react";
+
+
+export interface DataProps {
+    id: string;
+    nome: string;
+    amountSlice: number;
+    limitUse: number;
+    primaryColor: string;
+    secondaryColor: string;
+    textColor: string;
+    premios: string;
+}
+
+
+interface RouletteProps {
+    data: DataProps
+}
+
+
+export function DataRoulette({ data }: RouletteProps){
+
+    console.log("Data Rolette log", data)
+    const newPremios = data.premios?.split(',')
+
+    const [premios, setPremios] = useState<string[]>([])
+     
+
+    useEffect( () => {
+        async function getNames() {
+            const res = await api.post('/getnames2', {
+                id: data.premios
+            }).then((res) => {
+                console.log("res", res.data)
+                const names = res.data
+                setPremios(names.data)
+            })
+        }
+
+        getNames()
+    }, [])
+
+
+
+    console.log("array de premios id", premios)
+
+    
+    const newData = premios.map((item, index) => {
+        
+        if(index % 2 === 0){
+            return {
+                option: item,
+                style: {
+                    backgroundColor: data.primaryColor,
+                    textColor: data.textColor
+                }
+            }
+        }else {
+            return {
+                option: item,
+                style: {
+                    backgroundColor: data.secondaryColor,
+                    textColor: data.textColor
+                }
+            }
+        }
+    })
+
+    console.log("Dados para roleta",newData)
+
+    return (
+        <>{(newData.length !== 0 ) && <Roulette data={newData}/>}</>
+    )
+}
---
+++ src/components/Roulette.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+
+import React, { useState, useEffect } from 'react'
+import { Wheel } from 'react-custom-roulette'
+import { motion } from "framer-motion";
+import { api } from '@/lib/api';
+
+
+interface DataProps {
+    option: string;
+    style: {
+        backgroundColor: string;
+        textColor: string;
+    }
+}
+
+interface RouletteProps {
+    data: DataProps[]
+}
+
+export function Roulette({ data }: RouletteProps){
+
+
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+
+    const handleSpinClick = () => {
+        const newPrizeNumber = Math.floor(Math.random() * data.length)
+        console.log(newPrizeNumber)
+        setPrizeNumber(newPrizeNumber)
+        setMustSpin(true)
+    }
+
+
+
+
+    return (
+        <div>
+            <div className='flex relative p-2 rounded-full items-center justify-center bg-purple-500 shadow-black shadow-md'>
+                <Wheel
+                    fontWeight={700}
+                    fontSize={30}
+                    mustStartSpinning={mustSpin}
+                    radiusLineColor={'black'}
+                    prizeNumber={prizeNumber}
+                    data={data}
+                    innerRadius={20}
+                    radiusLineWidth={2}
+                    outerBorderWidth={0}
+                    onStopSpinning={() => {
+                        setMustSpin(false)
+                    }}
+                />
+                <motion.div
+                    className="absolute rounded-full w-[64px] h-[64px] border-2 bg-white flex items-center justify-center border-black"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                >
+                    <button className='text-xl font-bold font-mono ' onClick={handleSpinClick}>SPIN</button>
+                </motion.div>
+            </div>
+        </div>
+    )
+
+}
